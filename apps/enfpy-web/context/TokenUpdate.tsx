d76b6e0b545093bbef5f64c0052fc4f083808151
Jerry <jerry@cupist.com>
2023-08-31 10:01:07 +0900

    feat: 앱 로딩시 토큰 갱신 요청 추가

--- apps/enfpy-web/context/TokenUpdate.tsx
+++ apps/enfpy-web/context/TokenUpdate.tsx
@@ -11,12 +11,36 @@ import tokenUtil from "../utils/tokenUtil";
 const TokenUpdate = ({ children }: PropsWithChildren) => {
   const session = useSession();
   const auth = useAuth();
+  const isUnauthenticated =
+    !session.data && session.status === "unauthenticated";
 
+  /**
+   * localStorage에 refreshToken갱신
+   */
   useEffect(() => {
     if (session.data && auth.isSignIn) {
       tokenUtil.update(session.data.refreshToken);
     }
   }, [session.data, auth.isSignIn]);
+
+  /**
+   * 앱 시작시점에 session이 없는 경우 silentRefresh 요청
+   * @todo 현재 auth.silentRefresh에서 400에러 반환되고 있음 확인 필요
+   */
+  useEffect(() => {
+    const token = tokenUtil.get();
+    const refreshToken = token.refreshToken;
+
+    if (isUnauthenticated && refreshToken) {
+      tokenUtil.delete();
+      console.log("try silentRefresh");
+
+      // TODO: 400에서 확인 필요 message:invalid_request_error
+      // auth.silentRefresh(refreshToken, {
+      //   redirect: false,
+      // });
+    }
+  }, [isUnauthenticated, auth.silentRefresh]);
 
   return <>{children}</>;
 };
