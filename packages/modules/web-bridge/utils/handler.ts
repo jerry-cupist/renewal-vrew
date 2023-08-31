5aba770bbedd513680d11f158c51558b6cf466f2
Jerry <jerry@cupist.com>
2023-08-31 17:41:20 +0900

    feat: auth message handler 추가

--- packages/modules/web-bridge/utils/handler.ts
+++ packages/modules/web-bridge/utils/handler.ts
@@ -3,3 +3,9 @@ import { WebBridgeMessageHandler } from "../types/message";
 export const createMessageHandler = <P, R = void>(
   handler: WebBridgeMessageHandler<P, R>
 ) => handler;
+
+export const createMessageHandlers = <T extends string, P, R>(
+  handler: Record<T, WebBridgeMessageHandler<P, R>>
+) => {
+  return handler;
+};
