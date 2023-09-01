f383ba78ca77b4193332d86db0ced7ce7801e2b1
Jerry <jerry@cupist.com>
2023-09-01 14:01:39 +0900

    feat: 앱 라우팅 useNavagation통합

--- packages/modules/web-bridge/hooks/useNavigation.ts
+++ packages/modules/web-bridge/hooks/useNavigation.ts
@@ -1,17 +1,17 @@
 import { WebBridgeActionDatas, WebBridgeActions } from "../types/action";
 import { postMessage } from "../utils/message";
 
+const navigate = (
+  args: WebBridgeActionDatas[WebBridgeActions.NAVIGATION_NAVIGATE]
+) => {
+  postMessage(WebBridgeActions.NAVIGATION_NAVIGATE, args);
+};
+
+const goBack = () => {
+  postMessage(WebBridgeActions.NAVIGATION_GO_BACK);
+};
+
 export const useNavigation = () => {
-  const navigate = (
-    args: WebBridgeActionDatas[WebBridgeActions.NAVIGATION_NAVIGATE]
-  ) => {
-    postMessage(WebBridgeActions.NAVIGATION_NAVIGATE, args);
-  };
-
-  const goBack = () => {
-    postMessage(WebBridgeActions.NAVIGATION_GO_BACK);
-  };
-
   return {
     navigate,
     goBack,
