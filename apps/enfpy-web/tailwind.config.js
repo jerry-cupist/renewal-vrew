981aae4a348883e67e7727409682ce6b32167e87
Jerry <jerry@cupist.com>
2023-09-07 16:39:08 +0900

    fix: 테일윈드 파일 스캔 타임을 줄이기 위해 확장자 범위 축소

--- apps/enfpy-web/tailwind.config.js
+++ apps/enfpy-web/tailwind.config.js
@@ -1,12 +1,9 @@
 import sharedConfig from 'tailwind-config/tailwind.config.js'
 
-// const path = require('path')
+const path = require('path')
 
-// const UI_PACKAGE_PATH = path.dirname(require.resolve('@vrew/ui'))
-// const ABSOLUTE_UI_PACKAGE_PATH = path.join(
-//   UI_PACKAGE_PATH,
-//   '**/*.{js,ts,jsx,tsx}',
-// )
+const UI_PACKAGE_PATH = path.dirname(require.resolve('@vrew/ui'))
+const ABSOLUTE_UI_PACKAGE_PATH = path.join(UI_PACKAGE_PATH, '**/*.{jsx,tsx}')
 
 /**
  * @note 외부 패키지 참조시 빌드 시간이 오래 걸리는 이슈가 존재함.
@@ -15,11 +12,11 @@ const config = {
   presets: [sharedConfig],
   darkMode: 'class',
   content: [
-    './app/**/*.{ts,tsx}', // Note the addition of the `app` directory.
-    './context/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './features/**/*.{ts,tsx}',
-    // ABSOLUTE_UI_PACKAGE_PATH,
+    './app/**/*.{jsx,tsx}', // Note the addition of the `app` directory.
+    './context/**/*.{jsx,tsx}',
+    './components/**/*.{jsx,tsx}',
+    './features/**/*.{jsx,tsx}',
+    ABSOLUTE_UI_PACKAGE_PATH,
   ],
 }
 
