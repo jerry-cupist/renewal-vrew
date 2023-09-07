d8c0d318cbb92de1b4e67ffc7a84c61ee8479068
Jerry <jerry@cupist.com>
2023-09-07 15:01:49 +0900

    fix: 테일윈드에 외부 라이브러리에 대한 스캔을 추가할 경우 빌드시간이 길어짐

--- apps/enfpy-web/tailwind.config.js
+++ apps/enfpy-web/tailwind.config.js
@@ -1,17 +1,26 @@
 import sharedConfig from 'tailwind-config/tailwind.config.js'
 
-const path = require('path')
+// const path = require('path')
 
-const UI_PACKAGE_PATH = path.dirname(require.resolve('@vrew/ui'))
-const ABSOLUTE_UI_PACKAGE_PATH = path.join(
-  UI_PACKAGE_PATH,
-  '**/*.{js,ts,jsx,tsx}',
-)
+// const UI_PACKAGE_PATH = path.dirname(require.resolve('@vrew/ui'))
+// const ABSOLUTE_UI_PACKAGE_PATH = path.join(
+//   UI_PACKAGE_PATH,
+//   '**/*.{js,ts,jsx,tsx}',
+// )
 
+/**
+ * @note 외부 패키지 참조시 빌드 시간이 오래 걸리는 이슈가 존재함.
+ */
 const config = {
   presets: [sharedConfig],
   darkMode: 'class',
-  content: [ABSOLUTE_UI_PACKAGE_PATH],
+  content: [
+    './app/**/*.{ts,tsx}', // Note the addition of the `app` directory.
+    './context/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './features/**/*.{ts,tsx}',
+    // ABSOLUTE_UI_PACKAGE_PATH,
+  ],
 }
 
 export default config
