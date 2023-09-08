10939383a034f921245b959d8a760e84650af212
Jerry <jerry@cupist.com>
2023-09-08 15:38:12 +0900

    feat: 버튼 액티브 스타일링

--- apps/enfpy-web/features/profile/Chip.tsx
+++ apps/enfpy-web/features/profile/Chip.tsx
@@ -25,14 +25,24 @@ export default function Chip({
   const validColorClass = backgroundColor && !isColorCode(backgroundColor)
   const backgroundColorClass = validColorClass ? backgroundColor : ''
   const colorClass = color || 'text-black'
+  const afterElementClass = [
+    'after:content-[""]',
+    'after:absolute',
+    'after:inset-0',
+    'after:pointer-events-none',
+  ].join(' ')
+  const activeClass =
+    'active:bg-[gray] active:after:bg-[#1a1a3e] active:after:opacity-10'
 
   return (
     <button
       className={clsx(
-        'flex items-center h-[28px] p-[8px] rounded-[6px] gap-[4px]',
+        'select-none relative flex items-center h-[28px] p-[8px] rounded-[6px] gap-[4px] overflow-hidden',
         fullWidth && 'w-full',
         backgroundColorClass,
         colorClass,
+        afterElementClass,
+        activeClass,
       )}
       {...props}
     >
