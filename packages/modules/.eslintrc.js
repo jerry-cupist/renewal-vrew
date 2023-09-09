6c0903adfcbeb25b6dd00f9434ecdf1a369467f6
Jerry <jerry@cupist.com>
2023-09-09 13:14:26 +0900

    chore: modules packages eslint설정

---
+++ packages/modules/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: ['custom/typescript'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off', // todo: any 제거하기
+  },
+}
--- packages/modules/enfpyBridge/shared/constants/page-enpfy.ts
+++ packages/modules/enfpyBridge/shared/constants/page-enpfy.ts
@@ -1,12 +1,14 @@
 export const ENFPY_WEB_URL = {
-  ROOT: "/",
-  LOGIN: "/login",
-  PROFILE: "/profile",
-  MAIN: "/main",
-  MAIN_MBTI: "/main/mbti",
-  SUB: "/sub",
-} as const;
+  ROOT: '/',
+  LOGIN: '/login',
+  PROFILE: '/profile',
+  MAIN: '/main',
+  MAIN_MBTI: '/main/mbti',
+  SUB: '/sub',
+  CHAT_LIST: '/chat-list',
+  STATION: '/station',
+} as const
 
-export type EnfpyWebUrlType = typeof ENFPY_WEB_URL;
-export type EnfpyPathnameType = keyof EnfpyWebUrlType;
-export type EnfpyWebPathnameType = EnfpyWebUrlType[EnfpyPathnameType];
+export type EnfpyWebUrlType = typeof ENFPY_WEB_URL
+export type EnfpyPathnameType = keyof EnfpyWebUrlType
+export type EnfpyWebPathnameType = EnfpyWebUrlType[EnfpyPathnameType]
